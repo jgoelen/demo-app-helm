ddb490ee176371c76a1ba1b14b3e91eae1d138e2
Jurgen Goelen <jurgen.goelen@aca-it.be>
2022-01-27 15:22:37 +0100

    Add script to update image

---
+++ update_image.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+export IMAGE=$1
+git pull
+yq e -i '.app.image = strenv(IMAGE)' ./config/test/values.yaml
+git add ./config/test/values.yaml
+git commit -m "Update image to $IMAGE"
